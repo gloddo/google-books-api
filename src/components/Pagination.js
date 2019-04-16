5e121ce73bbede5461ef540f95570c5076fe1cef
Joe Makkio <makkio94@gmail.com>
2019-04-16 16:23:51 +0200

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,13 +3,15 @@ import { KeyboardArrowLeft, KeyboardArrowRight } from "@material-ui/icons";
 
 function Pagination(props) {
   return (
-    props.totalPages !==0 && (<div className="change-page">
-      <KeyboardArrowLeft onClick={() => props.changePage(-12)} />
-      <span>
-        Page {props.actualPage+1 || 0} - {props.totalPages+1 || 0}
-      </span>
-      <KeyboardArrowRight onClick={() => props.changePage(12)} />
-    </div>)
+    props.totalPages !==0 && (
+      <div className="change-page">
+        <KeyboardArrowLeft onClick={() => props.changePage(-12)} />
+        <span>
+          Page {props.actualPage+1 || 0} - {props.totalPages+1 || 0}
+        </span>
+        <KeyboardArrowRight onClick={() => props.changePage(12)} />
+      </div>
+    )
   );
 }
 
